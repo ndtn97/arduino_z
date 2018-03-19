abdef2a0465dc2019b760ab01ededf05d2567786
nadayan <32271862+nadayan@users.noreply.github.com>
2018-03-19 18:04:35 +0100

    some improvement

--- pc_script/main.py
+++ pc_script/main.py
@@ -3,8 +3,10 @@ import threading
 import csv
 from serial.tools import list_ports
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+import math
 import numpy as np
+
+pi=math.pi
 
 helpdialog ="csv:write data to csv file\r\nexit:close this app\r\ndict:output data to console\r\nread:output raw serial input"
 
@@ -16,12 +18,12 @@ parameters = ['id', 'an0', 'an0_min', 'an0_max', 'an1', 'an1_min', 'an1_max']
 WATCH_SER_PORT = False
 STREAM_ON = True
 
-def wavePlotter(bufsize, x, y0, y1):
-    fig = plt.figure()
-    ax=fig.add_subplot(111)
-    #ax.cla()
-    ax.plot(x,y0,marker = '.', linestyle = '-', color = '#fa4b00', label = 'an0')
-    ax.plot(x, y1, marker='.', linestyle='-', color='#0b225d', label='an1')
+def wavePlotter(ax,bufsize, x, y0, y1):
+
+    #while 1:
+    ax.cla()
+    ax.plot(x,y0,marker = ',', linestyle = '-', color = '#fa4b00', label = 'an0')
+    ax.plot(x, y1, marker=',', linestyle='-', color='#0b225d', label='an1')
 
     # x and y lables
     ax.set_title('Input Volt')
@@ -50,41 +52,38 @@ def wavePlotter(bufsize, x, y0, y1):
     ax.yaxis.grid(True, which = 'major', linestyle = '-', color = '#CFCFCF')
     ax.set_axisbelow(True)
 
-    plt.show()
+    plt.pause(.01)
 
-
-
+    #plt.show()
 
 class serThread(threading.Thread):
-    def __init__(self,valdict,phase_t,toPLT,updated):
+    def __init__(self):
         super(serThread, self).__init__()
         self.input_line = ''
-        self.dict = valdict
-        self.comms = ''
-        self.ph_t = phase_t
-        self.toPLT = toPLT
-        self.updated = updated
+        self.dict = [[{'id':i,} for i in range(BUFSIZE)], [{'id':i,} for i in range(BUFSIZE)]] #Ping Pong Buffer
+        self.pFlag = 0 #Ping Pong Buffer Flag
+        self.ph_t = [0.0 for i in range(3)]
+        self.watch_ser_port = False
+        self.stream_on = True
     def run(self):
-        tmpdict=self.dict.copy()
         count = 0
         while 1:
-            if(STREAM_ON):
+            if(self.stream_on):
                 try:
                     self.input_line = ser.readline().decode().replace('\r\n', '')
-                    if WATCH_SER_PORT:
+                    if self.watch_ser_port:
                         print(self.input_line)
                     if self.input_line[0] != '#':
-                        self.updated = False
                         input_vals = self.input_line.split(',')
                         input_vals = [float(i) for i in input_vals]
                         count=int(input_vals[6])
                         ToDict = {'an0': input_vals[0], 'an0_min': input_vals[1], 'an0_max': input_vals[2], 'an1': input_vals[3], 'an1_min': input_vals[4], 'an1_max': input_vals[5]}
-                        toPLT[0][count] = input_vals[0]
-                        toPLT[1][count] = input_vals[3]
-                        tmpdict[count].update(ToDict)
+                        self.dict[self.pFlag][count].update(ToDict)
                         if count == int(BUFSIZE - 1):
-                            self.dict = tmpdict.copy()
-                            self.updated = True
+                            if self.pFlag == 0:
+                                self.pFlag = 1
+                            elif self.pFlag == 1:
+                                self.pFlag = 0
                     elif self.input_line[1] == 'p':
                         t_vals = self.input_line[3:].split(',')
                         for i in range(3):
@@ -92,12 +91,29 @@ class serThread(threading.Thread):
                 except:
                     pass
 
+    def getData(self):#returns 256 sample data [{'id':0,'an0':0.1,'an1':0.4,...},{'id':1,...},{},...]
+        if self.pFlag == 0:
+            flag = 1
+        elif self.pFlag == 1:
+            flag = 0
+        return self.dict[flag]
+    
+    def getPeakTime(self):#returns [an0 PeakTime[microsec],an1 PeakTime[mictosec],an0PeakT-an1PeakT[mictosec]]
+        return self.ph_t
+
+    def setWatchSerPort(self,flag):
+        self.watch_ser_port = flag
+    
+    def setStreamFlag(self, flag):
+        self.stream_on = flag
+        
+
 def findPort():
     try:
         uno = next(list_ports.grep("Arduino Uno"))
-        print('Device Found:'+ uno.device )
-        DEV_NAME = uno.device
-        return DEV_NAME
+        print('Device Found:' + uno.device + ' ' + uno.product )
+        dev_name = uno.device
+        return dev_name
     except StopIteration:
         print('No device found')
         exit()
@@ -112,41 +128,100 @@ def outToCSV(rows):
         for d in rows:
             writer.writerow(d)
 
+def calcFreq(data, period):
+    # This Function From
+    # http://www.contec-kb.com/wp/wp-content/uploads/2013/08/1355_syuhasu_kensyutu.pdf
+
+    thold = 2.5
+    all_cnt = 0
+    k_mode = 0
+    up1_cnt = 0
+    dw_cnt = 0
+    up2_cnt = 0
+    finished = False
+    samptm = 0.000001*period
+    freq = 0.0
+
+    while finished == False:
+        all_cnt += 1
+        if k_mode == 0:
+            if data[all_cnt] < thold:
+                k_mode = 1
+        elif k_mode == 1:
+            if data[all_cnt] >= thold:
+                k_mode = 2
+                up1_cnt = all_cnt
+        elif k_mode == 2:
+            if data[all_cnt] < thold:
+                k_mode = 3
+                dw_cnt = all_cnt
+        elif k_mode == 3:
+            if data[all_cnt] >= thold:
+                k_mode = 4
+                up2_cnt = all_cnt
+        
+        if(all_cnt >= (BUFSIZE - 1)) or (k_mode == 4):
+            finished = True
+            
+    freq = 1 / ((up2_cnt - up1_cnt) * samptm)
+
+    return freq
+
+def calcPhaseDiff(diff_t, w_freq):
+    w_period=float(1/w_freq)
+    ph_diff_rad = float((diff_t / w_period) * 2 * pi)
+    return ph_diff_rad
+
+def antiailiasing(bufsize, s_data, s_period):
+    #s_data:waveformdata,s_period:sampring period[s],freq_c:cut-off frequency[hz]
+    freq_c = (10 ** 6) / (2 * s_period)
+    s_period *= 0.000001
+    print(str(s_data)+' '+str(s_period)+' '+str(freq_c))
+    t = np.arange(0, bufsize * s_period, s_period)
+    freq = np.linspace(0, 1.0 / s_period, bufsize)
+    
+    F = np.fft.fft(s_data)
+    F = F / (bufsize / 2)
+    F[0] = F[0] / 2
+    F2 = F.copy()
+    print(F2)
+    F2[(freq > freq_c)] = 0
+    print(F2)
+    f2 = np.fft.ifft(F2)
+    f2 = np.real(f2 * bufsize)
+    return f2
+
 if __name__ == '__main__':
-    DEV_NAME=findPort()
+    DEV_NAME = findPort()
     print("all commands for type \"help\"")
-    valdict = [{'id':i,} for i in range(BUFSIZE)]
-    phase_t = [0.0 for i in range(3)]
     ser = serial.Serial(DEV_NAME, BAUD_RATE)
     #ser.dtr = False
 
-    DRAW_REF = False
-    toPLT = [[i for i in range(BUFSIZE)], [i for i in range(BUFSIZE)]]
 
-    #ani = animation.FuncAnimation(fig, wavePlotter, interval=100,fargs=(ax,BUFSIZE,[i for i in range(BUFSIZE)], toPLT[0], toPLT[1]))
+    period = 50
+    #Plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot([i for i in range(BUFSIZE)],[0 for i in range(BUFSIZE)])
+    lines,=ax.plot(0,0)
 
-
-
-    #drawHandler = drawThread(BUFSIZE, toPLT[0], toPLT[1], DRAW_REF)
-    #drawHandler.setDaemon(True)
-    #drawHandler.start()
-
-    serialListener = serThread(valdict,phase_t,toPLT,DRAW_REF)
+    serialListener = serThread()
     serialListener.setDaemon(True)
     serialListener.start()
 
     while 1:
         #Key input
         key = input()
-        if key == 'csv':#write data to csv
-            #print(valdict)
-            outToCSV(serialListener.dict)
+        if key == 'csv': #write data to csv
+            data = serialListener.getData()
+            outToCSV(data)
         elif key == 'exit':#exit app
             exit()
         elif key == 'dict':#output dictionary data
-            print(serialListener.dict)
+            print(serialListener.getData())
         elif key == 'read':#output raw serial port data
             WATCH_SER_PORT = not(WATCH_SER_PORT)
+            serialListener.setWatchSerPort(WATCH_SER_PORT)
         elif key == 'help':
             print(helpdialog)
         elif key == 'speriod':
@@ -156,16 +231,32 @@ if __name__ == '__main__':
                 ser.write(str(period).encode('utf-8'))
             else:
                 period = 50
-                print('50[mictosec]<period')
+                print('50[microsec] < period')
             print("sampling period set at" + str(period) + "[microsec]")
         elif key == 'ph':
-            print(phase_t)
+            print(serialListener.getPeakTime())
         elif key == 'reset':
             ser.write('1'.encode('utf-8'))
         elif key == 'stream':
             STREAM_ON = not(STREAM_ON)
+            serialListener.setStreamFlag(STREAM_ON)
             print('STREAMING:' + str(STREAM_ON))
         elif key == 'plot':
-            print('notice:no commands while showing plot')
-            wavePlotter(BUFSIZE,[i for i in range(BUFSIZE)], toPLT[0], toPLT[1])
+            try:
+                data = serialListener.getData()
+                #filterd = antiailiasing(BUFSIZE,[data[i]['an0'] for i in range(BUFSIZE)], period)
+                #wavePlotter(ax, BUFSIZE, [i for i in range(BUFSIZE)], filterd, [2.5 for i in range(BUFSIZE)])
+                wavePlotter(ax,BUFSIZE, [i for i in range(BUFSIZE)], [data[i]['an0'] for i in range(BUFSIZE)], [data[i]['an1'] for i in range(BUFSIZE)])
+            except:
+                print("No data Wait a minute...")
+        elif key == 'f':
+            try:
+                data=serialListener.getData()
+                freq = calcFreq([data[i]['an0'] for i in range(BUFSIZE)], period)
+                print(format(freq, '.2f') + '[Hz]' + format(1 / freq) + '[s]')
+                t = serialListener.getPeakTime()
+                ph_d_rad = calcPhaseDiff(float(t[2]), freq)
+                print(format(ph_d_rad,'.2f')+'[rad]')
+            except:
+                print("No data Wait a minute...")
         key = ''
